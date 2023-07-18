62035e8164becad453735733545e0812d9655f20
Rohit Singh <rohit.singh1@adglobal360.com>
2023-07-18 11:33:18 +0530

    new implementation in Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,11 @@
 public class Diff {
     
+     public static void main(String[]args){
+    
+        int d =47;
+        int e =9;
+    
+        System.out.println("the product is"+(d-e));
+    } 
+
 }
